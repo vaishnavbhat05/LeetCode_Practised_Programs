a21267ebbb6ae0703596b61b9fc2f064f596e767
Vaishnav <104012712+vaishnavbhat05@users.noreply.github.com>
2023-11-06 23:38:45 +0530

    Create Merge_Strings_Alternative.java

---
+++ Merge_Strings_Alternative.java
@@ -0,0 +1,73 @@
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+// Example 1:
+
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+// Example 2:
+
+// Input: word1 = "ab", word2 = "pqrs"
+// Output: "apbqrs"
+// Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+// word1:  a   b 
+// word2:    p   q   r   s
+// merged: a p b q   r   s
+// Example 3:
+
+// Input: word1 = "abcd", word2 = "pq"
+// Output: "apbqcd"
+// Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+// word1:  a   b   c   d
+// word2:    p   q 
+// merged: a p b q c   d
+ 
+
+// Constraints:
+
+// 1 <= word1.length, word2.length <= 100
+// word1 and word2 consist of lowercase English letters.
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first word: ");
+        String word1 = sc.nextLine();
+
+        System.out.print("Enter the second word: ");
+        String word2 = sc.nextLine();
+
+        String merged = mergeAlternately(word1, word2);
+        System.out.println("Merged string: " + merged);
+
+        sc.close(); // Don't forget to close the Scanner when you're done.
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder merged = new StringBuilder();
+        int i = 0, j = 0;
+
+        while (i < word1.length() && j < word2.length()) {
+            merged.append(word1.charAt(i++));
+            merged.append(word2.charAt(j++));
+        }
+
+        while (i < word1.length()) {
+            merged.append(word1.charAt(i++));
+        }
+
+        while (j < word2.length()) {
+            merged.append(word2.charAt(j++));
+        }
+
+        return merged.toString(); // Return the merged string.
+    }
+}
+
+  

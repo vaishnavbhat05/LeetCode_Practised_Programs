62683fa4a64b2676240a351658cff4080e6e22c3
Vaishnav <104012712+vaishnavbhat05@users.noreply.github.com>
2023-11-08 22:36:28 +0530

    Create Great_Numbers.java

---
+++ Great_Numbers.java
@@ -0,0 +1,63 @@
+// Example 1:
+
+// Input: candies = [2,3,5,1,3], extraCandies = 3
+// Output: [true,true,true,false,true] 
+// Explanation: If you give all extraCandies to:
+// - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+// - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+// - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+// - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+// - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+// Example 2:
+
+// Input: candies = [4,2,1,1,2], extraCandies = 1
+// Output: [true,false,false,false,false] 
+// Explanation: There is only 1 extra candy.
+// Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
+// Example 3:
+
+// Input: candies = [12,1,12], extraCandies = 10
+// Output: [true,false,true]
+ 
+
+// Constraints:
+
+// n == candies.length
+// 2 <= n <= 100
+// 1 <= candies[i] <= 100
+// 1 <= extraCandies <= 50
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = 0;
+        for (int candy : candies) {
+            maxCandies = Math.max(maxCandies, candy);
+        }
+        
+        List<Boolean> result = new ArrayList<>();
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= maxCandies);
+        }
+        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int[] candies1 = {2, 3, 5, 1, 3};
+        int extraCandies1 = 3;
+        System.out.println(solution.kidsWithCandies(candies1, extraCandies1)); // Output: [true, true, true, false, true]
+        
+        int[] candies2 = {4, 2, 1, 1, 2};
+        int extraCandies2 = 1;
+        System.out.println(solution.kidsWithCandies(candies2, extraCandies2)); // Output: [true, false, false, false, false]
+        
+        int[] candies3 = {12, 1, 12};
+        int extraCandies3 = 10;
+        System.out.println(solution.kidsWithCandies(candies3, extraCandies3)); // Output: [true, false, true]
+    }
+}

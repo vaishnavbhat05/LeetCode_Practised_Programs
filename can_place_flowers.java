c40dc4d05299860b36b9177610d7009caca55cea
Vaishnav Bhat <104012712+vaishnavbhat05@users.noreply.github.com>
2024-10-08 00:00:33 +0530

    Update can_place_flowers.java

--- can_place_flowers.java
+++ can_place_flowers.java
@@ -12,3 +12,18 @@ Example 2:
 
 Input: flowerbed = [1,0,0,0,1], n = 2
 Output: false
+ 
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int m = flowerbed.length;
+        for (int i = 0; i < m; ++i) {
+            int l = i == 0 ? 0 : flowerbed[i - 1];
+            int r = i == m - 1 ? 0 : flowerbed[i + 1];
+            if (l + flowerbed[i] + r == 0) {
+                flowerbed[i] = 1;
+                --n;
+            }
+        }
+        return n <= 0;
+    }
+}
